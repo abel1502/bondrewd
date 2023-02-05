3ab5e68c711c9b9be3acb6da156f3e9704da882b
abel1502 <terragame.abel@gmail.com>
2023-02-06 00:08:05 +0300

    src_location

---
+++ include/bondrewd/src_location.hpp
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <cstddef>
+#include <utility>
+#include <string>
+#include <string_view>
+#include <fmt/core.h>
+
+
+namespace bondrewd::lex {
+
+
+struct SrcLocation {
+public:
+    #pragma region Fields
+    std::string_view filename{""};
+    unsigned line{0};
+    unsigned column{0};
+    #pragma endregion Fields
+
+    #pragma region Constructors
+    SrcLocation() = default;
+
+    SrcLocation(std::string_view filename, unsigned line, unsigned column) :
+        filename{filename}, line{line}, column{column} {}
+    #pragma endregion Constructors
+
+    #pragma region Service constructors
+    SrcLocation(const SrcLocation &) = default;
+    SrcLocation(SrcLocation &&) = default;
+    SrcLocation &operator=(const SrcLocation &) = default;
+    SrcLocation &operator=(SrcLocation &&) = default;
+    #pragma endregion Service constructors
+
+    #pragma region to_string
+    std::string to_string(bool detailed = false) const {
+        if (filename.empty() || !detailed) {
+            return fmt::format("{}:{}", line, column);
+        }
+
+        return fmt::format("{}:{} in {}", filename, line, column);
+    }
+    #pragma endregion to_string
+
+};
+
+
+}  // namespace bondrewd::lex
