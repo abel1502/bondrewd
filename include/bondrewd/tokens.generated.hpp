a9e2280eebdede8f60a6330988f74179bbf6607d
abel1502 <terragame.abel@gmail.com>
2023-02-06 00:08:28 +0300

    Begin token

---
+++ include/bondrewd/tokens.generated.hpp
@@ -0,0 +1,19 @@
+// Placeholder; will be replaced by the generated file.
+
+#pragma once
+
+
+namespace bondrewd::lex {
+
+
+enum class Punct {
+    //
+};
+
+
+enum class HardKeyword {
+    //
+};
+
+
+}  // namespace bondrewd::lex
---
+++ src/tokens.generated.cpp
@@ -0,0 +1,2 @@
+// Placeholder; will be replaced by the generated file.
+
