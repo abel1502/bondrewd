adc9b60304f129282bd7c0cfc4af2e9decf839ab
abel1502 <terragame.abel@gmail.com>
2023-02-07 15:59:02 +0300

    Add token-related code generation

---
+++ tools/tokens/generate_tokens.py
@@ -0,0 +1,145 @@
+from __future__ import annotations
+import typing
+import argparse
+import pathlib
+import textwrap
+import jinja2
+import datetime
+import dataclasses
+import codecs
+
+
+from trie_gen import Trie, TrieInfo
+
+
+LINE_COMMENTS: typing.Final[typing.Collection[str]] = ("//", "#")
+BLOCK_COMMENT: typing.Final[typing.Tuple[str, str]] = ("/*", "*/")
+STRING_QUOTES: typing.Final[typing.Collection[str]] = ("'", '"')
+STRING_ESCAPES: typing.Final[typing.Collection[str]] = ("'", '"', "n", "r", "t", "0", "\\")
+
+
+parser = argparse.ArgumentParser(
+    description=textwrap.dedent("""
+    The script responsible for generating several token-related source files.
+    
+    More precisely, the script generates the following files:
+     - include/bondrewd/lex/tokens.generated.hpp
+     - src/lex/tokens.generated.cpp
+    """)
+)
+
+parser.add_argument(
+    "-k", "--keywords",
+    type=pathlib.Path,
+    help="The path to the keywords listing file.",
+    default=pathlib.Path(__file__).parent.parent.parent / "grammar/keywords",
+)
+
+parser.add_argument(
+    "-p", "--puncts",
+    type=pathlib.Path,
+    help="The path to the punctuations listing file.",
+    default=pathlib.Path(__file__).parent.parent.parent / "grammar/puncts",
+)
+
+parser.add_argument(
+    "-o", "--output",
+    type=pathlib.Path,
+    help="The path to the root of the interpreter directory.",
+    default=pathlib.Path(__file__).parent.parent.parent,
+)
+
+
+@dataclasses.dataclass
+class ListingItem:
+    name: str
+    value: str
+    
+    @classmethod
+    def from_line(cls, line: str) -> ListingItem:
+        name, value = line.strip().split(maxsplit=1)
+        return cls(name, value)
+
+
+Listing = typing.List[ListingItem]
+
+
+def read_listing(path: pathlib.Path) -> Listing:
+    with path.open("r") as f:
+        return [ListingItem.from_line(line) for line in f]
+
+
+@dataclasses.dataclass
+class TokensInfo:
+    keywords: Listing
+    puncts: Listing
+    punct_lookup: typing.Dict[str, str]
+    misc_trie: TrieInfo
+    string_trie: TrieInfo
+    block_comment_trie: TrieInfo
+    
+    @classmethod
+    def process(cls, keywords: Listing, puncts: Listing) -> TokensInfo:
+        punct_lookup = {punct.value: punct.name for punct in puncts}
+        
+        misc_trie = Trie()
+        string_trie = Trie()
+        block_comment_trie = Trie()
+        
+        misc_trie.add_wordlist(LINE_COMMENTS, "line_comment")
+        misc_trie.add_word(BLOCK_COMMENT[0], "block_comment")
+        misc_trie.add_wordlist(punct_lookup.keys(), "punct")
+        misc_trie.add_wordlist(STRING_QUOTES, "string_quote")
+        
+        string_trie.add_wordlist(("\\" + k for k in STRING_ESCAPES), "escape")
+        string_trie.add_wordlist(STRING_QUOTES, "end_quote")
+        
+        block_comment_trie.add_word(BLOCK_COMMENT[0], "start")
+        block_comment_trie.add_word(BLOCK_COMMENT[1], "end")
+
+        return cls(
+            keywords,
+            puncts,
+            punct_lookup,
+            TrieInfo.from_trie(misc_trie),
+            TrieInfo.from_trie(string_trie),
+            TrieInfo.from_trie(block_comment_trie),
+        )
+
+
+def render_templates(output_dir: pathlib.Path, tokens_info: TokensInfo):
+    env = jinja2.Environment(
+        loader=jinja2.FileSystemLoader(pathlib.Path(__file__).parent / "templates"),
+        autoescape=False,
+    )
+    
+    env.filters["cpp_escape"] = lambda s: codecs.encode(s, "unicode_escape").decode("ascii").replace("'", "\\'")
+    
+    env.get_template("tokens.tpl.hpp").stream(
+        date=datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S UTC"),
+        tokens_info=tokens_info,
+    ).dump(str(output_dir / "include/bondrewd/lex/tokens.generated.hpp"))
+    
+    env.get_template("tokens.tpl.cpp").stream(
+        date=datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S UTC"),
+        tokens_info=tokens_info,
+    ).dump(str(output_dir / "src/lex/tokens.generated.cpp"))
+
+
+def main():
+    args = parser.parse_args()
+    
+    keywords_file: pathlib.Path = args.keywords
+    puncts_file: pathlib.Path = args.puncts
+    output_dir: pathlib.Path = args.output
+    
+    keywords: typing.List[typing.Tuple[str, str]] = read_listing(keywords_file)
+    puncts: typing.List[typing.Tuple[str, str]] = read_listing(puncts_file)
+    
+    tokens_info = TokensInfo.process(keywords, puncts)
+    
+    render_templates(output_dir, tokens_info)
+
+
+if __name__ == "__main__":
+    exit(main())
---
+++ tools/tokens/trie_gen.py
@@ -0,0 +1,142 @@
+from __future__ import annotations
+import typing
+import dataclasses
+from functools import cached_property
+
+
+# class WordChecker:
+#     _node: Node
+#     _word: typing.List[str]
+    
+#     def __init__(self, node: Node):
+#         self._node = node
+#         self._word = []
+    
+#     def is_term(self) -> bool:
+#         return self._node.is_term()
+    
+#     def go(self, ch: str) -> None:
+#         assert len(ch) <= 1
+        
+#         if not ch:
+#             return
+        
+#         self._node = self._node.get_child(ch)
+#         self._word.append(ch)
+    
+#     def restart(self) -> None:
+#         self._node = self._node.master._root
+#         self._word = []
+    
+#     def get_verdict(self) -> typing.Any:
+#         return self._node.verdict
+    
+#     def get_word(self) -> str:
+#         return "".join(self._word)
+
+
+@dataclasses.dataclass(eq=False)
+class Node:
+    verdict: typing.Any
+    master: Trie
+    parent: Node | None = None
+    word: str = ""
+    children: typing.Dict[str, Node] = dataclasses.field(default_factory=dict)
+    # Fields below are autofilled
+    index: int = None
+    
+    def __post_init__(self):
+        if self.index is None:
+            self.index = self.master._allocate_node_index()
+    
+    def is_term(self) -> bool:
+        return not self.children
+    
+    def get_child(self, ch: str) -> Node:
+        assert len(ch) == 1
+        
+        return self.children.get(ch, self.master._sentinel)
+    
+    def add_child(self, ch: str, verdict: typing.Any) -> Node:
+        return self.children.setdefault(ch, Node(verdict, self.master, self, self.word + ch))
+    
+    @cached_property
+    def rollback(self) -> Node:
+        """
+        Rollback is a (non-strict) parent node that has a positive verdict, if any.
+        It is needed for the cases where a word is a prefix of another word.
+        
+        For example, if we have two puncts: `=` and `=!>`, and get `=!a` as input,
+        when we see the `a`, we will use rollback to get the verdict for `=`
+        instead of reporting an error.
+        """
+        
+        if self.verdict is not self.master._verdict_none:
+            return self
+        
+        if self.parent is None:
+            return self.master._sentinel
+        
+        return self.parent.rollback
+
+
+class Trie:
+    _verdict_none: typing.Final[typing.Any]
+    _next_node_index: int
+    _root: typing.Final[Node]
+    _sentinel: typing.Final[Node]
+    _all_nodes: typing.Set[Node]
+    
+    def __init__(self, verdict_none: typing.Any = "none"):
+        self._verdict_none = verdict_none
+        self._next_node_index = 0
+        self._root = Node(verdict_none, self)
+        self._sentinel = Node(verdict_none, self)
+        self._all_nodes = {self._root, self._sentinel}
+    
+    def add_word(self, word: str, verdict: typing.Any) -> None:
+        verdict_none = self._verdict_none
+        
+        node = self._root
+        for ch in word:
+            node = node.add_child(ch, verdict_none)
+            self._all_nodes.add(node)
+        
+        if node.verdict != verdict_none:
+            raise ValueError(f"Word {word!r} already exists in the trie")
+        
+        node.verdict = verdict
+    
+    def add_wordlist(self, words: typing.Iterable[str], verdict: typing.Any) -> None:
+        for word in words:
+            self.add_word(word, verdict)
+    
+    # def create_checker(self) -> WordChecker:
+    #     return WordChecker(self._root)
+    
+    def _allocate_node_index(self) -> int:
+        index = self._next_node_index
+        self._next_node_index += 1
+        return index
+
+
+@dataclasses.dataclass
+class TrieInfo:
+    verdicts: typing.List[str]
+    root: Node
+    
+    @classmethod
+    def from_trie(cls, trie: Trie) -> TrieInfo:
+        verdicts: typing.Set[str] = set()
+        
+        for node in trie._all_nodes:
+            verdict = node.verdict
+            assert isinstance(verdict, str), f"All verdicts must be strings, not {verdict!r}"
+            verdicts.add(verdict)
+        
+        assert trie._verdict_none in verdicts
+        verdicts.remove(trie._verdict_none)
+        
+        verdicts = [trie._verdict_none] + list(verdicts)
+        
+        return cls(verdicts, trie._root)
