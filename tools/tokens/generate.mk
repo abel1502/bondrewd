3ab916588d946189097699b9f05348e08eba5c90
abel1502 <terragame.abel@gmail.com>
2023-02-10 18:29:35 +0300

    Unify codegen tools interface

---
+++ tools/tokens/generate.mk
@@ -0,0 +1,16 @@
+GENERATED_TOKENS_HPP := $(PROJECT_ROOT)/include/bondrewd/lex/tokens.generated.hpp 
+GENERATED_TOKENS_CPP := $(PROJECT_ROOT)/src/lex/tokens.generated.cpp
+GENERATED += $(GENERATED_TOKENS_HPP) $(GENERATED_TOKENS_CPP)
+
+
+_DEPENDENCIES := \
+	$(CURRENT_PATH)/templates/tokens.tpl.hpp \
+	$(CURRENT_PATH)/templates/tokens.tpl.cpp \
+	$(CURRENT_PATH)/generate_tokens.py \
+	$(CURRENT_PATH)/trie_gen.py \
+	$(CONFIG_KEYWORDS) \
+	$(CONFIG_PUNCTS)
+
+
+$(GENERATED_TOKENS_HPP) $(GENERATED_TOKENS_CPP): $(_DEPENDENCIES)
+	$(PYTHON) $(CURRENT_PATH)/generate_tokens.py --keywords $(CONFIG_KEYWORDS) --puncts $(CONFIG_PUNCTS) --output $(PROJECT_ROOT)
